8d1226e759293a4f152870ed1f1ed88ea301ca5b
mr366manish <141211862+mr366manish@users.noreply.github.com>
2024-02-16 17:15:46 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,31 @@
+var inputvalue = document.querySelector('#citt')
+var btn = document.querySelector('#add');
+var city = document.querySelector('#cityoutput');
+var descrip = document.querySelector('#description')
+var temp = document.querySelector('#temp')
+var wind = document.querySelector('#wind')
+apik = "139ccd11eb827664973ff6309ddc6568"
+function convertion(val)
+{
+    return (val - 273).toFixed(3)
+}
+btn.addEventListener('click', function()
+{
+    fetch('https://api.openweathermap.org/data/2.5/weather?q='+inputvalue.value+'&appid='+apik)
+    .then(res => res.json())
+
+    .then(data =>
+        {
+            var nameval = data['name']
+            var descrip = data['weather']['0']['description']
+            var tempature = data['main']['temp']
+            var wndspeed = data['wind']['speed']
+
+            city.innerHTML=`Weather of <span>${nameval}<span>`
+            temp.innerHTML = `Temperature : <span>${convertion(tempature)} C<span>`
+            description.innerHTML = `Sky conditions: <span>${descrip}<span>`
+            wind.innerHTML= `Wind Speed : <span> ${wndspeed} km/h<span>`
+        })
+
+        .catch(err => alert('You entered Wrong city Name'))
+})
